c11daf38081b8255af1b9fbf1661ab453fd71dc5
Bishal Bhat <bishalbhat2002@gmail.com>
2025-04-23 17:24:33 +0545

    add student role to the system

--- src/student/dashboard.php
+++ src/student/dashboard.php
@@ -1,30 +1,150 @@
 <?php require_once "../../includes/header.php"; ?>
 
+<!-- Code for Updating Student Info-->
+<?php
+       if(isset($_GET['update-student-info'])){           
+?>
+              <div class="main center-fdct">
+                     <h1 class="heading">Update Student Info</h1>
+                     <form action="?" method="post" name="Update-student-form" enctype="multipart/form-data" class="form-expan small-input-field">
+                           
+                            <div class="col-span-2 center mb-2">
+                                   <img src="../../assets/images/image.jpg" alt="Photo" class="image small-image">
+                            </div> 
+
+                            <input type="hidden" name="regd-no" value="">      
+                            
+                            <div>
+                                   <label for="name">Name:</label>
+                                   <input type="text" name="name" id="name">
+                            </div>
+                            <div>
+                                   <label for="dob">DOB:</label>
+                                   <input type="date" name="dob" id="dob">
+                            </div>      
+                            <div class="gender">
+                                   <label for="gender">Gender:</label><br>
+                                   <input type="radio" name="gender" value="male">Male
+                                   <input type="radio" name="gender" value="female">Female
+                            </div>  
+                            <div>
+                                   <label for="phone">Phone:</label>
+                                   <input type="number" name="phone" id="phone">
+                            </div>
+                            <div>
+                                   <label for="email">Email:</label>
+                                   <input type="email" name="email" id="email">
+                            </div>                      
+                            <div>
+                                   <label for="address">Address:</label>
+                                   <input type="text" name="address" id="address">
+                            </div> 
+                            <div>
+                                   <label for="parent-name">Parent:</label>
+                                   <input type="text" name="parent-name" id="parent-name">
+                            </div>                                
+                            <div>
+                                   <label for="parent-phone">Parent Phone:</label>
+                                   <input type="number" name="parent-phone" id="parent-phone">
+                            </div>                                
+
+                            <div> 
+                                   <label for="photo">Upload Photo:</label>
+                                   <input type="file" name="photo" id="photo">
+                            </div> 
+                            <div>
+                                   <label for="view-results">View Results:</label> <br>
+                                   <div id="view-results"> 
+                                          <a href="" class="view-btn btn">SEE Result</a>
+                                          <a href="" class="view-btn btn">NEB Result</a>
+                                   </div>
+                            </div>            
+                            <div>
+                                   <label for="see-result">SEE Result:</label>
+                                   <input type="file" name="see-result" id="see-result">
+                            </div>
+                            <div>
+                                   <label for="neb-result">NEB Result:</label>
+                                   <input type="file" name="neb-result" id="neb-result">
+                            </div>
+
+                            <div class="col-span-2 center">
+                                   <button class="save-btn btn large mt-1">Save Changes</button>
+                            </div>  
+                     </form>
+              </div>
+ 
+
+<!-- Code for Updating Student Password-->
+<?php
+       }else if(isset($_GET['update-student-password'])){           
+?>
+              <div class="main center-fdct">
+                     <h1 class="heading">Update Password</h1>
+                     <form action="" name="update-student-password-from" method="post" enctype="multipart/form-data" class="form">
+                            <input type="hidden" name="tid" value="">
+                            <div>
+                                   <label for="currentPassword">current Password:</label> <br>
+                                   <input type="text" name="currentPassword" value="">
+                            </div>
+                            <div>
+                                   <label for="newPassword">New Password:</label> <br>
+                                   <input type="text" name="newPassword" value="">
+                            </div>
+                            <div>
+                                   <label for="confirmNewPassword">Confirm New Password:</label> <br>
+                                   <input type="text" name="confirmNewPassword" value="">
+                            </div>
+                            <div class="center btn-container">
+                                   <input type="submit" value="Save Changes" class="update-btn">
+                            </div>
+                     </form>
+              </div>
 
 
+<!-- Code To display Student Dashboard... -->
+<?php
+       }else{
+?> 
+              <div class="center-fdct main">
+              <div class="teacher-dashboard center-fdct">
+                     <div class="greet">
+                            <h1>Welcome Student !!</h1>
+                            <h3>Bishal Bhat</h3>    
+                     </div>
 
+                     <div class="teacher-info">
+                            <div>
+                                   <img src= <?php echo BASE_URL."/assets/images/image.jpg"; ?> alt="profile picture">
+                            </div>
+                                   <p>Name: Bishal Bhat</p>
+                                   <p>Gender: Male</p>
+                                   <p>DOB: 2000-12-19</p>
+                                   <p>Faculty: Department of CSIT</p>
+                                   <p>Phone: 98XXXXX</p>
+                                   <p>Email: bishalbhat2002@gmail.com</p>
+                                   <p>Address: Mahendranagar, kanchanpur</p>
+                                   <p>Teacher ID: 1001</p>
+                                   <p class="center"><a href="?update-student-info" class="update-btn">update Info</a>
+                                   <a href="?update-student-password" class="update-btn">update Password</a></p>
+                            </div>
 
+                            <div>
+                                   <div>
+                                          <div class="total-attendance"></div>
+                                          <div class="current-semester"></div>
+                                   </div>
+                                   <div class="new-notices">
 
+                                   </div>
+                            </div>
+              </div>
+              </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php
+       }
+?>
 
 <?php require_once "../../includes/footer.php"; ?>
+
+
--- src/student/study-materials.php
+++ src/student/study-materials.php
@@ -1,30 +1,36 @@
 <?php require_once "../../includes/header.php"; ?>
 
+<!-- Code to view study materials -->
 
+      <div class="main center-fdct">
+              <div class="center-fdct">
+                     <h1 class="heading">Semester X, View Study Materials</h1>
+                     
+                     <form action="?view-attendance-sem" name="sem-select-form" method="POST" enctype="multipart/form-data" class="study-material-container">
+                            <div class="id">ID</div>
+                            <div class="subject-title">Web Technology - II</div>
+                            <div class="id-value">1021</div>
+                            <div class="main-content">
+                                   <p>The file linked to this message contains PHP pdf.</p>
+                                   <div class="space-between">
+                                          <div>
+                                                 <a href="../../uploads/study materials/PHP.pdf" download="sampleNote" class="save-btn">Click To Download</a>
+                                                 <a href="../../uploads/study materials/PHP.pdf" target="_blank" class="view-btn">View Note</a>
+                                                 
+                                          </div>
+                                          <div class="flex gap-2">
+                                                 <span class="teacher-name">Bishal Bhat</span>
+                                                 <span class="sent-date"><?php echo date('Y-m-d');?></span>
+                                          </div>
+                                   </div>
+                            </div>
+                            <div class="actions">
+                                   <a href="?update-study-material-id" class="update-btn">update</a><br>
+                                   <a href="?update-study-material-id" class="delete-btn" onclick="return confirmDelete(id)">Delete</a>
+                            </div>
+                     </form>                                                
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+              </div>
+              </div> 
 
 <?php require_once "../../includes/footer.php"; ?>
